a1914ba05f847b435e42387858fc90526f0f900d
krishnendra007 <krishnendrasharma@gmail.com>
2024-09-06 22:13:26 +0530

    adding customized security chain

---
+++ src/main/java/com/springsecuity/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.springsecuity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+
+        /*http.csrf(customizer ->customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        //http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        */
+
+        http.csrf(customizer ->customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //http.formLogin(Customizer.withDefaults());
+
+        /*Customizer<CsrfConfigurer<HttpSecurity>> custcsrf= new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+                customizer.disable();
+            }
+        }*/
+        return http.build();
+    }
+}
